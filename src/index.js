8b0ad4611686e01cfc905a845ce7da2534316100
Karolis Davli <kdavlijevas@gmail.com>
2021-12-04 11:15:33 +0100

    project list render

--- src/index.js
+++ src/index.js
@@ -1,19 +1,75 @@
-import _ from 'lodash';
 import "./styles/styles.css"
-import { addProject } from "./addProject"
-import addTodo from "./addTodo"
-import browseP from "./browseP"
 
 // Content elements
-const content = document.getElementById('content');
+const listsContainer = document.querySelector('[data-lists]')
+const newListFrom = document.querySelector('[data-new-list-form]')
+const newListInput = document.querySelector('[data-new-list-input]')
+const deleteListButton = document.querySelector('[data-delete-list-button]')
+
+const LOCAL_STORAGE_LIST_KEY = 'tasks.lists'
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'tasks.selectedListId'
+
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
+
+listsContainer.addEventListener('click', e => {
+  if (e.target.tagName.toLowerCase() === 'li') {
+    selectedListId = e.target.dataset.listId
+    saveAndRender();
+  }
+})
+
+deleteListButton.addEventListener('click', e => {
+  lists = lists.filter(list => list.id !== selectedListId)
+  selectedListId = null
+  saveAndRender()
+})
 
 
+newListFrom.addEventListener('submit', e => {
+  e.preventDefault()
+  const listName = newListInput.value
+  if (listName == null || listName === '') return
+  const list = createList(listName)
+  newListInput.value = null
+  lists.push(list)
+  saveAndRender()
+})
+
+function createList(name) {
+  return { id: Date.now().toString(), name: name, tasks: [] }
+}
+
+function saveAndRender() {
+  save()
+  render()
+}
+
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
+}
+
+function render() {
+  clearElement(listsContainer)
+  lists.forEach(list => {
+    const listElement = document.createElement('li')
+    listElement.dataset.listId = list.id
+    listElement.classList.add('list-item')
+    listElement.innerHTML = list.name
+    if (list.id === selectedListId) {
+      listElement.classList.add('active')
+    }
+    listsContainer.appendChild(listElement)
+  })
+}
 
 
-// Initialise page
-addProject();
-// addTodo();
+function clearElement(el) {
+  while (el.firstChild) {
+    el.removeChild(el.firstChild)
+  }
+}
 
 
-
-
+render()
